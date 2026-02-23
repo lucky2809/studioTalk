7b59c409b4dc3b1a6a12e2783888ffcbf3bf0b95
Rohit <rohit.sangod74@gmail.com>
2026-02-23 11:32:44 +0530

    add dashboard layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Login from './components/pages/Login'
 import Signup from './components/pages/Signup'
 import Home from './components/pages/Home'
 import Shop from './components/shop/Shop'
+import DashboardRoot from './components/Admin/Dashboard/DashboardRoot'
 
 function App() {
 
@@ -15,6 +16,12 @@ function App() {
           <Route path='/shop' element={<Shop />} />
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<Signup />} />
+          <Route path="/dashboard" element={<DashboardRoot />}>
+            {/* <Route index element={<OverviewCards />} />
+            <Route path="analytics" element={<Anaylitics />} />
+            <Route path="widget" element={<ChatWidget />} /> */}
+
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
